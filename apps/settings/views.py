0e6e62afc81b981dd91cc37ff16d6f604a755b4b
AbrorIT <sultanovabror578@gmail.com>
2023-01-16 21:52:02 +0600

    urls

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from apps.settings.models import Setting
 
 # Create your views here.
+
+
+def index(request):
+    home = Setting.objects.latest('id')
+    context = {
+        'home' : home,
+    }
+    return render(request, 'index.html', context)
+
